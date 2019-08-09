3475657ae471e7b08bd2fe9ab066906c2ba6c0d9
payne911 <jeremi.grenier.berthiaume@gmail.com>
2019-08-09 05:10:38 -0400

    gitignore update (LibGDX integration)

---
+++ core/src/com/payne/games/MainMenuScreen.java
@@ -0,0 +1,68 @@
+package com.payne.games;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class MainMenuScreen implements Screen {
+
+    final AriseDeeper game;
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final AriseDeeper game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to AriseDeeper!!! ", 200, 250);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 200, 200);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
