c601d9481c1ef448b8b40afa30c1ee358924e0fe
payne911 <jeremi.grenier.berthiaume@gmail.com>
2019-08-19 15:43:56 -0400

    `MainMenuScreen` is set up, and `scene2d.ui` integrated.

--- core/src/com/payne/games/screens/MainMenuScreen.java
+++ core/src/com/payne/games/screens/MainMenuScreen.java
@@ -90,7 +90,7 @@ public class MainMenuScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        stage.act(delta); // todo: possibly not necessary?
+        stage.act(delta);
         stage.draw();
     }
 
--- desktop/src/com/payne/games/desktop/DesktopLauncher.java
+++ desktop/src/com/payne/games/desktop/DesktopLauncher.java
@@ -9,8 +9,8 @@ import com.payne.games.logic.GameLogic;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = GameLogic.GAME_NAME;
-		config.width = GameLogic.GAME_WIDTH;
+		config.title  = GameLogic.GAME_NAME;
+		config.width  = GameLogic.GAME_WIDTH;
 		config.height = GameLogic.GAME_HEIGHT;
 		new LwjglApplication(new AriseDeeper(), config);
 	}
