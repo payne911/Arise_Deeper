52c52422e6302e470d76aa419a0ea9124a471fe9
payne911 <38117856+payne911@users.noreply.github.com>
2019-09-20 23:24:06 -0400

    Update SightSystem.java

--- core/src/com/payne/games/lightingSystem/SightSystem.java
+++ core/src/com/payne/games/lightingSystem/SightSystem.java
@@ -5,18 +5,18 @@ import com.payne.games.logic.GameLogic;
 import com.payne.games.map.BaseMapLayer;
 import com.payne.games.map.tiles.Tile;
 
-import static com.payne.games.logic.GameLogic.SUBDIVISIONS;
-// Used a static import because SUBDIVISIONS shows up so often...
+import static com.payne.games.logic.GameLogic.SUBDIVISIONS; // static because SUB shows up often
+
 
 /**
- * Subcell visibility system, tracking light using {@link FieldOfView} but with a larger grid and vision radius to smooth out blocks.
- * <br>
+ * Subcell visibility system, tracking light using {@link FieldOfView} but with a larger 
+ * grid and vision radius to smooth out blocks.
+ * 
  * todo:  see  https://www.redblobgames.com/articles/visibility/
  * todo: coupling/dependency could be reduced to the "Tile" class instead
  * todo: possibly do  https://stackoverflow.com/questions/45948732/2d-tile-based-smooth-lighting/45949202#45949202
  */
 public class SightSystem {
-
     private double[][] resistance; // assign 1.0 to positions that block vision
     private double[][] visible;    // this gets filled with alpha overlay values
 
@@ -25,11 +25,14 @@ public class SightSystem {
     private int widthBig;  // width times the subdivision amount; how many subcells wide the map is
     private int heightBig; // height times the subdivision amount; how many subcells high the map is
     
+    
     public SightSystem() {
     }
     
+    
     /**
-     * The latest visibility grid, as a 2D double array with larger dimensions than the map by a factor of the subdivision amount.
+     * The latest visibility grid, as a 2D double array with larger dimensions 
+     * than the map by a factor of the subdivision amount.
      */
     public double[][] getVisible() {
         return visible;
@@ -58,8 +61,8 @@ public class SightSystem {
         for (int i = 0; i < height; i++) {        // height
             for (int j = 0; j < width; j++) {     // width
                 if (currLevel.getTile(j, i).isSeeThrough()) {
-                    // this step may not be necessary; if a cell can change from disallowing moves to allowing moves,
-                    // then this is needed.
+                    /* todo: This step may not be necessary; if a cell can change from 
+                    disallowing moves to allowing moves, then this is needed. */
                     for (int y = 0; y < SUBDIVISIONS; y++) {
                         for (int x = 0; x < SUBDIVISIONS; x++) {
                             // sets a 5x5 block when SUBDIVISIONS is 5
@@ -79,16 +82,20 @@ public class SightSystem {
     }
 
     /**
-     * Updates both the Line of Sight of the hero, and the corresponding Fog of War.
+     * Updates both the Line of Sight of the hero, and the corresponding Fog of War.<br>
      * Manages the "explored" variable of the Tiles as well.
      */
     public void updateLighting(BaseMapLayer currLevel, int player_X, int player_Y, int sightRangeRadius) {
         updateResistanceMap(currLevel);
-        // Since resistance and visible use a larger map size, we offset the player position into the center subcell of that larger map.
-        // {@code player_X * SUBDIVISIONS} would put the player on an edge subcell; adding {@code SUBDIVISIONS / 2} puts him in the middle.
-        // sight radius must also be multiplied by SUBDIVISIONS to see the correct distance. There may be quirks with the actual radius because
-        // of how FieldOfView considers radius 1 to only include the player (I think), but radius 5 (for SUBDIVISIONS == 5) would see halfway
-        // into the next adjacent cell. A possible tweak would be to give as the last parameter: {@code sightRangeRadius * SUBDIVISIONS - SUBDIVISIONS / 2}.
+        /*
+        Since resistance and visible use a larger map size, we offset the player position into the center 
+        subcell of that larger map. {@code player_X * SUBDIVISIONS} would put the player on an edge subcell; 
+        adding {@code SUBDIVISIONS / 2} puts him in the middle.
+        Sight radius must also be multiplied by SUBDIVISIONS to see the correct distance. There may be quirks 
+        with the actual radius because of how FieldOfView considers radius 1 to only include the player 
+        (I think), but radius 5 (for SUBDIVISIONS == 5) would see halfway into the next adjacent cell. 
+        A possible tweak would be to give as the last parameter: {@code sightRangeRadius * SUBDIVISIONS - SUBDIVISIONS / 2}.
+        */
         FieldOfView.reuseFOV(resistance, visible, player_X * SUBDIVISIONS + SUBDIVISIONS / 2, player_Y * SUBDIVISIONS + SUBDIVISIONS / 2, sightRangeRadius * SUBDIVISIONS);
         Tile currTile;
         for(int i=0; i < height; i++) {          // height
@@ -97,16 +104,16 @@ public class SightSystem {
                 float alpha = 0; // we consider the fog of war alpha of a cell as the greatest visibility of any of its subcells.
                 for (int y = 0; y < SUBDIVISIONS; y++) {
                     for (int x = 0; x < SUBDIVISIONS; x++) {
-                        if(visible[i*SUBDIVISIONS+y][j*SUBDIVISIONS+x] > 0.0) // if an individual subcell is visible...
-                        {
+                        if(visible[i*SUBDIVISIONS+y][j*SUBDIVISIONS+x] > 0.0) { // if an individual subcell is visible...
                             currTile.setExplored(true); // consider the whole cell as explored.
                             // again, the alpha for a cell is the max of all its subcell alpha values.
                             alpha = Math.max(alpha, (float)visible[i*SUBDIVISIONS+y][j*SUBDIVISIONS+x]);
-                            // note, we can't use the average here because walls have only their edge subcells lit.
+                            // note: we can't use the average here because walls have only their edge subcells lit.
                         }
                     }
                 }
-                // per cell, we set the alpha to a value in the appropriate range.
+                
+                /* Per cell, we set the alpha to a value in the appropriate range. */
                 currTile.setFogAlpha(
                         MathUtils.map(0f, 1f,
                                 GameLogic.LOS_MIN_ALPHA, 1f,
